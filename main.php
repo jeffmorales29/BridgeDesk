f2076d91bd37e9db90ebffcb9a296ff645156635
jeffmorales29 <jeffmorales29@yahoo.com>
2024-03-04 12:49:21 +0800

    add enqueue admin

--- main.php
+++ main.php
@@ -17,12 +17,16 @@ License : GPLv2 or Later
     die;
  }
 
- class wphelpdesk
+ class bridgedesk
 
  {
 
    function __construct(){
       add_action('init', array( $this, 'custom_post_type') );
+   }
+
+   function register(){
+      add_action('admin_enqueue_scripts', array($this,'enqueue'));
    }
 
     function activate(){
@@ -39,19 +43,26 @@ License : GPLv2 or Later
     }
 
     function custom_post_type(){
-      register_post_type( 'ticket', ['public' => true, 'label' => 'Tickets']);
+      register_post_type( 'ticket', ['public' => true, 'label' => 'BridgeDesk']);
+    }
+
+    function enqueue() {
+      //enqueue all scripts
+      wp_enqueue_style('mypluginstyle', plugins_url('/assets/style.css',__FILE__));
+      wp_enqueue_script('mypluginscript', plugins_url('/assets/scripts.js',__FILE__));
     }
 
 
  }
 
- if(class_exists ('wphelpdesk')){
-    $wphelpdesk = new wphelpdesk();
+ if(class_exists ('bridgedesk')){
+    $bridgedesk = new bridgedesk();
+    $bridgedesk->register();
  }
 
 
  //Activation
-register_activation_hook( __FILE__, array( $wphelpdesk, 'activate' ));
+register_activation_hook( __FILE__, array( $bridgedesk, 'activate' ));
 
 //Deactivation
-register_deactivation_hook( __FILE__, array( $wphelpdesk, 'deactivate' ));
+register_deactivation_hook( __FILE__, array( $bridgedesk, 'deactivate' ));
